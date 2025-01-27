bbecdec75f56297376b34846e4449a48d634df9f
Kalel Jamil Alves <kalel.alves@outlook.com>
2025-01-27 20:58:33 -0300

    add: Create class User

---
+++ LibraryManager.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Core.Entities
+{
+    public class User : DevFreela.Core.Entities.BaseEntity
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        DateTime BirthDate { get; set; }
+
+    }
+}
