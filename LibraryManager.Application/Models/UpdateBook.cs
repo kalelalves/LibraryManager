86237916d9ad53a21e1a143b983239faae4ab5fd
Kalel Jamil Alves <73508900+kalelalves@users.noreply.github.com>
2025-01-30 21:11:11 -0300

    refactor: Added attributes to the UpdateBook model.

--- LibraryManager.Application/Models/UpdateBook.cs
+++ LibraryManager.Application/Models/UpdateBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryManager.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace LibraryManager.Application.Models
 {
     public class UpdateBook
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        string Author { get; set; }
+
+        public string Publisher { get; set; }
+        public DateOnly Publication { get; set; }
+        public string ISBN { get; set; }
+
+        public Category CategoryId { get; set; }
+
+
+
+        \bl
     }
 }
